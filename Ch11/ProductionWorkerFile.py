9356c0b8778708c19333765f052eea573a4e52f5
Amanpreet Kaur <156719225+amanpreet1699@users.noreply.github.com>
2024-04-22 18:42:05 -0400

    Adding comments

--- Ch11/ProductionWorkerFile.py
+++ Ch11/ProductionWorkerFile.py
@@ -1,37 +1,49 @@
+# Define the class
 class Employee:
-    
+    # Using init method with variables and self
     def __init__(self, employeeName, employeeNumber):
         self.__employeeName = employeeName
         self.__employeeNumber = employeeNumber
         
+    # Defining the set_employeeName method
     def set_employeeName(self, employeeName):
         self.__employeeName = employeeName
         
+    # Defining the set_employeeNumber method
     def set_employeeNumber(self, employeeNumber):
         self.__employeeNumber = employeeNumber
         
+    # Defining the get_employeeName method
     def get_employeeName(self):
         return self.__employeeName
     
+    # Defining the get_employeeNumber method
     def get_employeeNumber(self):
         return self.__employeeNumber
+
     
+# Define the class
 class ProductionWorker(Employee):
     
+    # Using init method with variables and self
     def __init__(self, employeeName, employeeNumber, shiftNumber, hourlyRate):
         Employee.__init__(self, employeeName, employeeNumber)
         self.__shiftNumber = shiftNumber
         self.__hourlyRate = hourlyRate
         
+    # Defining the set_employeeName method
     def set_shiftNumber(self, shiftNumber):
         self.__shiftNumber = shiftNumber
         
+    # Defining the set_hourlyRate method
     def set_hourlyRate(self, hourlyRate):
         self.__hourlyRate = hourlyRate
         
+    # Defining the get_shiftNumber method
     def get_shiftNumber(self):
         return self.__shiftNumber
     
+    # Defining the get_hourlyRate method
     def get_hourlyRate(self):
         return self.__hourlyRate
     
--- Ch11/ShiftSupervisorFile.py
+++ Ch11/ShiftSupervisorFile.py
@@ -1,38 +1,49 @@
+# Define the class
 class Employee:
-    
+    # Using init method with variables and self
     def __init__(self, employeeName, employeeNumber):
         self.__employeeName = employeeName
         self.__employeeNumber = employeeNumber
         
+    # Defining the set_employeeName method
     def set_employeeName(self, employeeName):
         self.__employeeName = employeeName
         
+    # Defining the set_employeeNumber method
     def set_employeeNumber(self, employeeNumber):
         self.__employeeNumber = employeeNumber
         
+    # Defining the get_employeeName method
     def get_employeeName(self):
         return self.__employeeName
     
+    # Defining the get_employeeNumber method
     def get_employeeNumber(self):
         return self.__employeeNumber
+
     
-    
+# Define the class
 class ShiftSupervisor(Employee):
-    
+
+    # Using init method with variables and self
     def __init__(self, employeeName, employeeNumber, annualSalary, annualProdBonus):
         Employee.__init__(self, employeeName, employeeNumber)
         self.__annualSalary = annualSalary
         self.__annualProdBonus = annualProdBonus
         
+    # Defining the set_annualSalary method
     def set_annualSalary(self, annualSalary):
         self.__annualSalary = annualSalary
         
+    # Defining the set_annualProdBonus method
     def set_annualProdBonus(self, annualProdBonus):
         self.__annualProdBonus = annualProdBonus
         
+    # Defining the get_annualSalary method
     def get_annualSalary(self):
         return self.__annualSalary
     
+    # Defining the get_annualProdBonus method
     def get_annualProdBonus(self):
         return self.__annualProdBonus
     
